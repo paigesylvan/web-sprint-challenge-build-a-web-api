3b27b45fda714f7ee1b47b364e9d2f82b19ea56f
PaigeSylvan <paigesylvan@gmail.com>
2024-01-04 22:09:53 -0600

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,31 @@
 // add middlewares here related to actions
+const Actions = require("./actions-model");
+
+const checkActionId = async ( req, res, next ) => {
+    try {
+        const action = await Actions.get(params.id);
+        if (!action) {
+            res.status(404).json({
+                message: "Action not found",
+            });
+        } else {
+            req.action = action;
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+};
+
+const checkNewAction = (req, res, next) => {
+    const { notes, description, project_id } = req.body;
+    if ( !notes || !description || project_id === undefined) {
+        res.status(400).json({
+            message: "notes, description and project_id are required",
+        });
+    } else {
+        next()
+    }
+};
+
+module.exports = { checkActionId, checkNewAction }
